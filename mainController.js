a47f2f98b0a278ffe436af19f9095dd7c80c2b4e
Shisha <shisha225@gmail.com>
2015-01-02 03:46:55 +0800

    重構計算機初始化部分程式碼
    改為 $q(promise) 回呼以結構化程式
    初始相關程式由各Controller移至MainController

--- mainController.js
+++ mainController.js
@@ -1,18 +1,91 @@
 app.controller('mainController', [
 	'$scope',
+	'$q',
+	'$http',
+	'hashStorage',
+	'infamyStorage',
 
-function($scope) {
+function($scope, $q, $http, hashStorage, infamyStorage) {
 	
 	// ================================================================
 	// = Space
 	// ================================================================
 	
+	$scope.hashStorage = hashStorage;
+
 	$scope.display = {};
 
-	$scope.skillsCalculator = null;
+	$scope.skillsCalculator = new SkillsCalculator;
 
-	$scope.perkDecksCalculator = null;
+	$scope.perkDecksCalculator = new PerkDecksCalculator;
 	
+
+	// ================================================================
+	// = Load
+	// ================================================================
+
+	var promises = [];
+	
+	promises[promises.length] = getConfigFiles(
+		"skills/config.json",
+		
+		function(files, config) {
+			$scope.skillsCalculator = new SkillsCalculator
+			((function setupTierConfig(trees, treeConfig) {
+				trees.forEach(function(tree) {
+					tree.tiers.forEach(function(tier) {
+						var info = treeConfig.tierinfo[tier.tier];
+						for (var attr in info) {
+							tier[attr] = info[attr];
+						}
+					});
+				});
+
+				return trees;
+			})(files, config));
+		}
+	);
+
+	promises[promises.length] = getConfigFiles(
+		"perks/config.json",
+
+		function(files, config) {
+			$scope.perkDecksCalculator = new PerkDecksCalculator
+			((function setupPerksConfig(perks, config) {
+				perks.forEach(function (perk) {
+					perk.decks = [
+						perk.decks[0],
+						config.decksInfo[0],
+						perk.decks[1],
+						config.decksInfo[1],
+						perk.decks[2],
+						config.decksInfo[2],
+						perk.decks[3],
+						config.decksInfo[3],
+						perk.decks[4]
+					];
+				});
+
+				return perks;
+			})(files, config));
+		}
+	);
+
+	$q.all(promises).then(function(data) {
+		// Load Skills
+		hashStorage.setupSkillsCalculator($scope.skillsCalculator);
+		
+		// Load Perks
+		hashStorage.setupPerkDeckCalculator($scope.perkDecksCalculator);
+
+		// Load Infamys
+		hashStorage.setupInfamy(infamyStorage.infamyStatus);
+
+		// Renew Skills
+		infamyStorage.update($scope.skillsCalculator);
+		$scope.skillsCalculator.updateStatus();
+	});
+
 
 	// ================================================================
 	// = Method
@@ -22,4 +95,40 @@ function($scope) {
 		return $scope[name] = value;
 	}
 	
+
+	// ================================================================
+	// = Funs
+	// ================================================================
+
+	function getConfigFiles(path, callback) {
+
+		return $q(function(resolve, reject) {
+
+			getFile(path).then(function(config) {
+				var files = config.files;
+
+				var promises = files.map(function(file, index) {
+					return getFile(file);
+				});
+
+				$q.all(promises).then(function(files) {
+					resolve(callback(files, config));
+				});
+			});
+		});
+		
+		function getFile(file) {
+			return $q(function(resolve, reject) {
+				$http.get(file)
+					.success(function(data) {
+						resolve(data);
+					})
+					.error(function(data) {
+						reject(data);
+					});
+			});
+		}
+	}
+	
+
 }]);
--- skillsController.js
+++ skillsController.js
@@ -1,87 +1,13 @@
 app.controller('skillsController', [
 	'$scope',
-	'$http',
-	'hashStorage',
-	'infamyStorage',
 
-function($scope, $http, hashStorage, infamyStorage) {
+function($scope) {
 
 	// ================================================================
 	// = Onload
 	// ================================================================
 
 	$scope.set('display', {});
-
-	if ( ! ($scope.skillsCalculator instanceof SkillsCalculator)) {
-		init('skills/config.json');
-	}
-
-
-	// ================================================================
-	// = Init
-	// ================================================================
-	
-	function init(file) {
-
-		$scope.set('skillsCalculator', new SkillsCalculator);
-		loadFiles(file);
-
-		function loadFiles(file) {
-			
-			$http.get(file).success(function(config) {
-				
-				var files	= config.files;
-				
-				// 複製陣列
-				var temp	= files.slice(0);
-				var counter	= files.length;
-				
-				files.forEach(function(file, index) {
-					$http.get(file).success(function(data) {
-						temp[index] = data;
-						
-						counter--;
-						if (counter == 0) afterLoad(temp, config);
-					});
-				});
-
-			}); // end $http get
-		
-		}
-
-		function afterLoad(trees, config) {
-			// 初始階級設定資料
-			initTierConfig(trees, config);
-
-			// 建構計算機物件
-			var newInstance = new SkillsCalculator(trees);
-			
-			// 載入惡名
-			hashStorage.setupInfamy(infamyStorage.infamyStatus);
-			infamyStorage.update(newInstance);
-			
-			// 載入Hash技能資料
-			hashStorage.setupSkillsCalculator(newInstance);
-			newInstance.updateStatus();
-
-			// 存入命名空間
-			$scope.set('skillsCalculator', newInstance);
-		}
-
-		/**
-		 * 設定階層訊息
-		 */
-		function initTierConfig(trees, treeConfig) {
-			trees.forEach(function(tree) {
-				tree.tiers.forEach(function(tier) {
-					var info = treeConfig.tierinfo[tier.tier];
-					for (var attr in info) {
-						tier[attr] = info[attr];
-					}
-				});
-			});
-		}
-	}
 
 
 	// ================================================================
@@ -98,18 +24,18 @@ function($scope, $http, hashStorage, infamyStorage) {
 
 	$scope.resetTree = function(tree) {
 		tree.unset();
-		hashStorage.setTreeData(tree);
-		hashStorage.updateUrl();
+		$scope.hashStorage.setTreeData(tree);
+		$scope.hashStorage.updateUrl();
 	}
 	
 	$scope.resetAll = function() {
 
 		$scope.skillsCalculator.trees.forEach(function(tree) {
 			tree.unset();
-			hashStorage.setTreeData(tree);
+			$scope.hashStorage.setTreeData(tree);
 		});
 		
-		hashStorage.updateUrl();
+		$scope.hashStorage.updateUrl();
 	}
 
 	// ================================================================
@@ -147,15 +73,15 @@ function($scope, $http, hashStorage, infamyStorage) {
 	$scope.skillClick = function(skill, tier, tree) {
 		skill.unlock();
 		
-		hashStorage.setTreeData(tree);
-		hashStorage.updateUrl();
+		$scope.hashStorage.setTreeData(tree);
+		$scope.hashStorage.updateUrl();
 	}
 	
 	$scope.skillRemove = function(skill, tier, tree) {
 		skill.unset();
 		
-		hashStorage.setTreeData(tree);
-		hashStorage.updateUrl();
+		$scope.hashStorage.setTreeData(tree);
+		$scope.hashStorage.updateUrl();
 	}
 	
 	$scope.skillUpdate = function(skill, tier, tree) {
